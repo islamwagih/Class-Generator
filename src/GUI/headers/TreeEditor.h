679e0be2b5fafc89d31137081215a4825f32ed64
mohamedhossam01 <mohamed.hossam.0617@gmail.com>
2023-05-02 23:14:44 +0300

    CG-50 Add QValidators to constraints, and show error messages in statusbar in case of error

---
+++ src/GUI/headers/TreeEditor.h
@@ -0,0 +1,37 @@
+#ifndef TREEEDITOR_H
+#define TREEEDITOR_H
+
+#include <QTreeWidgetItem>
+#include <QObject>
+#include <QStatusBar>
+
+class TreeEditor : public QObject
+{
+    Q_OBJECT
+public:
+    QTreeWidgetItem *makeRow(QTreeWidgetItem *parent);
+    QTreeWidgetItem *makeRow(QTreeWidgetItem *parent, QString name, QString type, QPair<QString, QString> constraints);
+    void removeRow(QTreeWidgetItem *item);
+    void onComboboxChanged(QTreeWidgetItem *item);
+    static TreeEditor *getInstance();
+    static void init(QTreeWidget *tree, QStatusBar *statusBar);
+    void reset();
+    ~TreeEditor();
+
+private slots:
+    bool handleValidator(QLineEdit *edit, QString text);
+
+private:
+    QTreeWidget *tree;
+    // TODO: Add/Remove types depending on the type of the file type (JSON, INI, etc.)
+    QStringList paramTypes = {"int", "float", "string", "bool", "nested"};
+    TreeEditor(const TreeEditor &) = delete;
+    TreeEditor &operator=(const TreeEditor &) = delete;
+    TreeEditor(QTreeWidget *tree, QStatusBar *statusBar);
+    void createConstraintLineEdits(QTreeWidgetItem *item);
+    void setLineEditsValidator(QTreeWidgetItem *item);
+    static TreeEditor *instance;
+    QStatusBar *statusBar;
+    void displayMessageOnStatusBar(QString message, int timeout);
+};
+#endif // TREEEDITOR_H
