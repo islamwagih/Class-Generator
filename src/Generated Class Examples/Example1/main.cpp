f8c2634ed7e9bbf5c20c561eff204220a017719c
Islam Wagih <islamwagihe@gmail.com>
2023-05-02 08:37:20 +0300

    [fix] upated example

--- src/Generated Class Examples/Example1/main.cpp
+++ src/Generated Class Examples/Example1/main.cpp
@@ -1,13 +1,15 @@
-#include "myclass.hpp"
-#include "parser.hpp"
+#include "myclass.cpp"
 #include "vector"
 #include "string"
 #include <iostream>
+
+MyClass* MyClass::instance = nullptr; //initialize the static pointer
+
 int main()
 {
     MyClass::init("test.json");
-    MyClass *myClass = MyClass::getInstance();
-    std::vector<std::string> path = {Parser::RECT1, Parser::WIDTH};
+    MyClass*myClass = MyClass::getInstance();
+    std::vector<std::string> path = {"rect", "width"};
     std::cout << myClass->getVar<int>(path) << std::endl;
     myClass->setVar<int>(path, 5);
 }
