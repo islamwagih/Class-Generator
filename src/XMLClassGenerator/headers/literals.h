ebf9676d4ad3f4e2823aff7afef81594d10a3789
mohamedhossam01 <mohamed.hossam.0617@gmail.com>
2023-05-06 13:42:18 +0300

    CG-54 Add XML Class Generator

--- src/XMLClassGenerator/headers/literals.h
+++ src/XMLClassGenerator/headers/literals.h
@@ -1,10 +1,11 @@
 #ifndef LITERALS_H
 #define LITERALS_H
 
-#include <string>
-
-std::string INCLUDES = R"(
-#ifndef {NAME}_{EXTENSION}
+#include "FormattableString.h"
+namespace literals
+{
+    FormattableString INCLUDES =
+        R"(#ifndef {NAME}_{EXTENSION}
 #define {NAME}_{EXTENSION}
 
 #include <map>
@@ -16,106 +17,112 @@ std::string INCLUDES = R"(
 
 )";
 
-std::string CLASS_NAME = R"(
-    std::string {CLASS_NAME};
+    FormattableString CLASS_NAME =
+        R"(class {CLASS_NAME}
+{
 )";
 
-std::string VISIBILITY = R"(
-{VISIBILITY}:
+    FormattableString VISIBILITY =
+        R"({VISIBILITY}:
 )";
 
-std::string CONSTRAINTS_MAP = R"(
-    std::map<std::string, std::vector<std::string>> constraints = {
-        {CONSTRAINTS}
-    }; // Auto generated from UI
+    FormattableString CONSTRAINTS_MAP =
+        R"(    std::map<std::string, std::vector<std::string>> constraints = {
+{CONSTRAINTS}    }; // Auto generated from UI
+
 )";
 
-std::string CONSTRAINTS = R"(
-        {"{PATH}", {"{MIN}", "{MAX}"}},
+    FormattableString ONE_CONSTRAINT =
+        R"(        {"{PATH}", {"{MIN}", "{MAX}"}},
 )";
 
-std::string FILE_PATH = R"(
-    std::string filePath;
+    FormattableString FILE_PATH =
+        R"(    std::string filePath;
+
 )";
 
-std::string KEYS = R"(
-    {KEYS}
-)";
-
-std::string KEY = R"(
-    std::string {KEY};
-)";
-
-// this in private section of XMLParser
-std::string privateSection = R"(
-    std::string file_path;
-    pugi::xml_document getXMLDoc(){
-
+    // this in private section of XMLParser
+    FormattableString GET_XML_DOC =
+        R"(    pugi::xml_document getXMLDoc(){
         pugi::xml_document doc;
-        if(!doc.load_file(file_path.c_str())){
+        if(!doc.load_file(filePath.c_str())){
             throw std::runtime_error("XML File not found");
         }
         return doc;
     }
+
 )";
 
-std::string CONSTRUCTOR = R"(
-    {CLASS_NAME}(std::string filePath):file_path(std::move(filePath))
-    {
-        this->filePath = filePath;
-        {KEYS}
+    // private helper function used by isLeaf , applyConstraints
+    FormattableString CONCAT_PATH =
+        R"(    std::string concatPath(std::vector<std::string> path){
+        std::string full_path = "";
+        for (const auto &entry : path)
+        {
+            full_path += entry + ".";
+        }
+        // remove last dot
+        full_path = full_path.substr(0, full_path.size() - 1);
+        return full_path;
     }
+
 )";
 
-std::string GET_FROM_FILE = R"(
+    FormattableString STRING_LITERALS =
+        R"({STRING_LITERALS}
+)";
+
+    FormattableString ONE_STRING_LITERAL =
+        R"(    std::string {NAME};
+)";
+
+    FormattableString CONSTRUCTOR =
+        R"(    {CLASS_NAME}(std::string filePath) : filePath(std::move(filePath))
+    {
+{INITIALIZE_LITERALS}    }
+)";
+
+    FormattableString INITIALIZE_LITERALS =
+        R"({INITIALIZE_LITERALS})";
+
+    FormattableString ONE_INITIALIZE_LITERAL =
+        R"(        {NAME_UPPER} = "{NAME}";
+)";
+
+    FormattableString GET_FROM_FILE = R"(
     template <typename T>
-    T getFromFile(std::vector<std::string>& path)
+    T getFromFile(std::vector<std::string> &path)
     {
         pugi::xml_document doc = this->getXMLDoc();
         pugi::xml_node node = doc.child("root");
-        for (const auto& tag : path)
+        for (const auto &tag : path)
         {
             node = node.child(tag.c_str());
             if(!node){
                 throw std::runtime_error("XML Tag not found");
             }
         }
-
         std::stringstream ss(node.child_value());
         T value;
         ss >> value;
         return value;
-    }
-)";
-// private helper function used by isLeaf , applyConstraints
-std::string _CONTACT_PATH = R"(
+    })";
 
-    std::string full_path = "";
-    for (const auto &entry : path)
-    {
-        full_path += entry + ".";
-    }
-    // remove last dot
-    full_path = full_path.substr(0, full_path.size() - 1);
-    return full_path;
-)";
-
-std::string IS_LEAF = R"(
+    FormattableString IS_LEAF = R"(
     bool isLeaf(std::vector<std::string> path)
     {
-        std::string full_path = _contactPath(path);
+        std::string full_path = concatPath(path);
         return constraints.count(full_path) == 1;
-    }
-)";
+    })";
 
-std::string SET_IN_FILE = R"(
+    FormattableString SET_IN_FILE = R"(
     template <typename T>
-    bool setInFile(std::vector<std::string>& path, T value)
+    bool setInFile(std::vector<std::string> &path, T value)
     {
         pugi::xml_document doc = this->getXMLDoc();
         pugi::xml_node node = doc.child("root");
 
-        for (const auto& tag : path)
+        for (const auto &tag : path)
         {
             node = node.child(tag.c_str());
             if(!node){
@@ -123,15 +130,14 @@ std::string SET_IN_FILE = R"(
             }
         }
         node.text().set(value);
-        return doc.save_file(file_path.c_str());
-    }
-)";
+        return doc.save_file(filePath.c_str());
+    })";
 
-std::string APPLY_CONSTRAINTS = R"(
+    FormattableString APPLY_CONSTRAINTS = R"(
     template <typename T>
     bool applyConstraints(std::vector<std::string> path, T value)
     {
-        std::string full_path = _contactPath(path);
+        std::string full_path = concatPath(path);
     if (std::is_same < T, int> ::value) {
         std::stringstream ss;
         ss << value;
@@ -179,13 +185,13 @@ std::string APPLY_CONSTRAINTS = R"(
         return valid;
     }
     return true;
-    }
-)";
+    })";
 
-std::string END = R"(
+    FormattableString END = R"(
 };
 
-#endif // {NAME}_{EXTENSION}
+#endif // {CLASS_NAME}_{EXTENSION}
 )";
+} // namespace literals
 
 #endif // LITERALS_H
